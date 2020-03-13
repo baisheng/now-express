f60b64c28125ee1c86560272d771065e1686b471
baisheng <baisheng@gmail.com>
2020-03-13 16:49:23 +0800

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require("express");
 const app = express();
-
+const Pageres = require('pageres');
+const captureWebsite  = require('capture-website')
 const port = 5000;
 
 // Body parser
 app.use(express.urlencoded({ extended: false }));
 
 // Home route
-app.get("/", (req, res) => {
-  res.send("Welcome to a basic express App");
+app.get("/", async (req, res) => {
+  // const screenshot = await captureWebsite.buffer('https://zcool.com.cn', 'screenshot.png', {
+  //   emulateDevice: 'iPhone X'
+  // });
+  const mysceenshot = await new Pageres({ delay: 2 })
+  .src('http://baidu.com', ['1024x768'])
+  .run()
+  // const screenshot = await new Pageres({ delay: 2 })
+  // .src('http://baidu.com', [ '480x320', '1024x768', 'iphone 5s' ], { crop: true })
+  // // .src('https://sindresorhus.com', [ '1280x1024', '1920x1080' ])
+  // // .src('data:text/html,<h1>Awesome!</h1>', [ '1024x768' ])
+  // // .dest(__dirname)
+  // .run();
+  // console.log(mysceenshot)
+  res.contentType('image/jpeg');
+  res.send(mysceenshot[0]);
+  // res.send("Welcome to a basic express App");
 });
 
 // Mock API
