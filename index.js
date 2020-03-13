d5400e08ab83101a11b187659ee4829eee214139
baisheng <baisheng@gmail.com>
2020-03-13 22:14:11 +0800

    first commit

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const app = express();
-const Pageres = require('pageres');
+// const Pageres = require('pageres');
 // const captureWebsite  = require('capture-website')
 const port = 5000;
 const parse = require('url');
 const { getScreenshot } = require('./chromium');
 const { getInt, getUrlFromPath, isValidUrl } = require('./validator');
-const got = require('got');
+// const got = require('got');
 const cheerio = require('cheerio')
 // Body parser
 app.use(express.urlencoded({ extended: false }));
-const Readability = require('readability')
-const JSDOM = require("jsdom").JSDOM;
-const requestPromise = require("request-promise-native");
-const read = require('read-art')
+// const Readability = require('readability')
+// const JSDOM = require("jsdom").JSDOM;
+// const requestPromise = require("request-promise-native");
+// const read = require('read-art')
 // import htmlMetadata from 'html-metadata';
-const parseDublinCore = require('html-metadata').parseDublinCore;
-const htmlMetadata = require('html-metadata')
+// const parseDublinCore = require('html-metadata').parseDublinCore;
+// const htmlMetadata = require('html-metadata')
 /**
  * Gets the self full URL from the request
  *
@@ -37,7 +37,21 @@ app.get("*", async (req, res) => {
   console.log(req.originalUrl)
 
   try{
+   // const targetUrl = 'http://www.bloomberg.com/news/articles/2016-05-24/as-zenefits-stumbles-gusto-goes-head-on-by-selling-insurance'
+   //  console.log(req.url)
+    // const { pathname = '/', query = {} } = parse(req.url, true);
+    // const {pathname = '/', query = {}} = fullUrl(req)
+    // console.log(fullUrl(req))
+    // console.log('ss-s-s-s-s-s--s-')
+    // const {pathname} = fullUrl(req)
+    // const { type = 'png', quality, fullPage } = query;
+    // const targetUrl = getUrlFromPath(pathname);
+    // console.log(targetUrl)
+    // const qual = getInt(quality);
+    console.log('targeturl .......')
     const targetUrl = getUrlFromPath(req.originalUrl)
+    console.log(targetUrl)
+    console.log('targeturl .......')
     if (!isValidUrl(targetUrl)) {
       res.statusCode = 400;
       res.setHeader('Content-Type', 'text/html');
@@ -59,6 +73,8 @@ app.get("*", async (req, res) => {
         // require('metascraper-readability')()
       ])
       const got = require('got')
+      // const targetUrl = 'http://open.163.com/newview/movie/courseintro?newurl=/special/opencourse/englishs1.html'
+      // const { body: html, url } = await got(targetUrl, {rejectUnauthorized: false})
       const { body: html, url } = await got(targetUrl, {
         retry: 10
       })
@@ -92,6 +108,45 @@ app.get("*", async (req, res) => {
   }
 })
 // Home route
+app.get("/aabbcc", async (req, res) => {
+  // const screenshot = await captureWebsite.buffer('https://zcool.com.cn', 'screenshot.png', {
+  //   emulateDevice: 'iPhone X'
+  // });
+  // const mysceenshot = await new Pageres({ delay: 2 })
+  // .src('http://baidu.com', ['1024x768'])
+  // .run()
+  // const screenshot = await new Pageres({ delay: 2 })
+  // .src('http://baidu.com', [ '480x320', '1024x768', 'iphone 5s' ], { crop: true })
+  // // .src('https://sindresorhus.com', [ '1280x1024', '1920x1080' ])
+  // // .src('data:text/html,<h1>Awesome!</h1>', [ '1024x768' ])
+  // // .dest(__dirname)
+  // .run();
+  // console.log(mysceenshot)
+  // res.contentType('image/jpeg');
+  // res.send(mysceenshot[0]);
+  // res.send("Welcome to a basic express App");
+  try {
+    const { pathname = '/', query = {} } = parse(req.url, true);
+    const { type = 'png', quality, fullPage } = query;
+    const url = getUrlFromPath(pathname);
+    const qual = getInt(quality);
+    if (!isValidUrl(url)) {
+      res.statusCode = 400;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(`<h1>Bad Request</h1><p>The url <em>${url}</em> is not valid.</p>`);
+    } else {
+      const file = await getScreenshot(url, type, qual, fullPage);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', `image/${type}`);
+      res.end(file);
+    }
+  } catch (e) {
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Server Error</h1><p>Sorry, there was a problem</p>');
+    console.error(e.message);
+  }
+});
 
 // Mock API
 app.get("/users", (req, res) => {
